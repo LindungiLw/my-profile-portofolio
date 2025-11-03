f85c573b2056de075eb335382145f0cf6bc8e044
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-11-03 12:20:05 +0700

    My Flow Structure Profile

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Home from './pages/home';
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route></Route>
+        </Router>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/home.tsx
@@ -0,0 +1,18 @@
+function home() {
+  return (
+    <div>
+      {/* Bagian Header */}
+      <section className="header-section">
+        {/* Bagian 1 */}
+        <div></div>
+
+        {/* Bagian 2 */}
+        <div></div>
+      </section>
+
+      <section></section>
+      <section></section>
+      <section></section>
+    </div>
+  );
+}
