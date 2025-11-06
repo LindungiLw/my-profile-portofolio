c52453ea84dd9cfca1f5f54c8ef000626a1e3b53
RahmaLindungiLaowo <rahmalindungilaowo@gmail.com>
2025-11-06 15:25:51 +0700

    Add tailwindcss and build navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScrollProgress from "./components/ScrollProgress";
+import ScrollProgress from "./components/scrollprogress";
 import Home from "./pages/home";
+import { Navigation } from "./navigation";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen"></div>
-      <ScrollProgress />
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Home></Home>
-            </>
-          }
-        ></Route>
-      </Routes>
+      <div className="min-h-screen">
+        <ScrollProgress />
+        <Navigation />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home></Home>
+              </>
+            }
+          ></Route>
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,6 +1,6 @@
 import { motion } from "motion/react";
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const Navigation = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -15,5 +15,43 @@ export const Navigation = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return <motion.nav initial={{ y: -100 }} animate={{ y: 0 }}></motion.nav>;
+  useEffect(() => {
+    setIsMobileMenuOpen(false);
+    window.scrollTo(0, 0);
+  });
+
+  const menuItems = [{ name: "Home", path: "/" }];
+
+  return (
+    <motion.nav
+      initial={{ y: -100 }}
+      animate={{ y: 0 }}
+      transition={{ duration: 0.6 }}
+      className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 
+      `}
+    >
+      <div className="container mx-auto px-6 py-4">
+        <div className="flex item-center justify-between">
+          <Link to="/">
+            <motion.div
+              className="text-x1 md:text-2xl text-white cursor-pointer"
+              style={{ fontWeight: 700 }}
+              whileHover={{ scale: 1.05 }}
+            >
+              <span className="text-[#6EACDA]">RAHMA</span>
+              <span>&nbsp;LAOWO</span>
+            </motion.div>
+          </Link>
+
+          <div>
+            {menuItems.map((item, index) => (
+              <Link key={index} to={item.path}>
+                <motion.div></motion.div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </motion.nav>
+  );
 };
